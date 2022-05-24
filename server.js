80214affc6c416670a8af3ff642f61e94bdf2101
AjayBhatta16 <ajay.bhattacharyya.16@gmail.com>
2022-05-23 21:28:22 -0400

    Finished index.html

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'))
 
 let dataEditor = new DataEditor('./data.json')
 
